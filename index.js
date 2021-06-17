89573020940de9aa1490ce9fea2fab836ffcf1fa
SCHOOLX <manr25611@gmail.com>
2021-06-16 19:19:11 -0700

    this

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const app = express()
+const server = require('http').createServer(app)
+const path = require('path')
+const io = require('socket.io')(server)
+
+app.use(
+  express.static( path.join(__dirname, '/static') )
+)
+io.on('connection', socket => {
+  console.log('Some client connected')
+  socket.on('chat', message => {
+    io.emit('chat', {message, id: socket.id})
+  })
+})
+const port = process.env.PORT || 3000
+server.listen(port, ()=> {
+  console.log('listening on: ', port)
+})
